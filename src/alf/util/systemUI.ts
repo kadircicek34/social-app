a3f3d0e699b8f971995a8d5501c9a4c8bdf26e05
kadircicek34 <kadircicek13@icloud.com>
2025-07-18 21:36:59 +0300

    fix theme background access

--- src/alf/util/systemUI.ts
+++ src/alf/util/systemUI.ts
@@ -6,7 +6,10 @@ import {type Theme} from '../types'
 export function setSystemUITheme(themeType: 'theme' | 'lightbox', t: Theme) {
   if (isAndroid) {
     if (themeType === 'theme') {
-      SystemUI.setBackgroundColorAsync(t.atoms.bg.backgroundColor)
+      const color = t?.atoms?.bg?.backgroundColor
+      if (color) {
+        SystemUI.setBackgroundColorAsync(color)
+      }
     } else {
       SystemUI.setBackgroundColorAsync('black')
     }
